cf112656d40fe7b8ca23e4c62716e8fbb05d31f0
Juliano Padilha <juliano.padilha@mercadolivre.com>
2020-09-26 19:06:19 -0300

    adiciona babel-preset-react para interpretar jsx e adiciona config de sourcemap no webpack

--- examples/react-webpack/src/index.js
+++ examples/react-webpack/src/index.js
@@ -6,7 +6,9 @@ import { render } from 'react-dom';
 import Title from './app';
 
 render(
-  React.createElement(Title),
+  // Paraq que funciona no formato do JSX, é preciso ter o preset do babel para react instalado econfigurado no .babelrc
+  // React.createElement(Title),
+  <Titleb />,
   // Por que estamos usando um atributo data e não um id, como normalmente?
   // O problema de utilizar o id é que cada vez que você utiliza um id para um elemento HTML, ele gera uma variável global em window
   document.querySelector('[data-js="app"]')
--- examples/react-webpack/webpack.config.js
+++ examples/react-webpack/webpack.config.js
@@ -8,18 +8,20 @@ const path = require('path');
 
 // Exportamos um objeto utilizando o module.exports do CommonJS para o webpack fazer a leitura do nosso arquivo e gerar o bundle
 module.exports  = {
+  // Diz para o webpack gerar um mapa do arquivo original. Muito útil para debugar o código.
+  devtool: 'source-map',
   // Entrada com o caminho do nosso arquivo principal
   // O __dirname é uma variável global do Node
   // O path.join vai juntar os três valores para o caminho funcionar em qualquer sistema operacional
   entry: path.join(__dirname, 'src', 'index'),
   // Caminho de saída do que o webpack ler do entry
   output: {
-     path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
     //  Nome do arquivo que será gerado
-     filename: 'bundle.js',
+    filename: 'bundle.js',
     //  O publicPath é o local onde o webpack-dev-server ficar assistindo as nossas alterações nos arquivos de desenvolvimento
     // Quando exeutamos o webpack-dev-server, ele não vai gerar um arquivo físico, ele gera um arquivo virtual em memória e o publicPath diz onde esse arquivo vai residir
-     publicPath: '/static/'
+    publicPath: '/static/'
   },
   module: {
     // Cada loader diz para o Webpack o que ele deve fazer com cada tipo de arquivo
