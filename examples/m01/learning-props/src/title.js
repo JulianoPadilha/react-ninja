90a334f83fc504f2b26c87d491b2e2dbf70ca676
Juliano Padilha <juliano.padilha@mercadolivre.com>
2020-09-29 20:42:48 -0300

    Usa o getDefault props no Title

--- examples/m01/learning-props/src/title.js
+++ examples/m01/learning-props/src/title.js
@@ -3,9 +3,17 @@
 import React from 'react'
 
 const Title = React.createClass({
+  // O getDefaultProps define valores default para as props, com isso evitamos fazer validações direto no método render
+  getDefaultProps () {
+    return {
+      name: 'Desconhecido',
+      lastName: 'Sem sobrenome'
+    }
+  },
+
   render () {
     return (
-      <h1>Olá {this.props.name}!</h1>
+      <h1>Olá {this.props.name + ' ' + this.props.lastName}!</h1>
     )
   }
 })
