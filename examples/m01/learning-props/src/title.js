49832afdd7567ad753ee0ecc226955c2abcb675c
Juliano Padilha <juliano.padilha@mercadolivre.com>
2020-10-05 20:51:42 -0300

    transformando o componente title em uma função pura e altera o defaultProps para um método estático

--- examples/m01/learning-props/src/title.js
+++ examples/m01/learning-props/src/title.js
@@ -2,20 +2,31 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  // O getDefaultProps define valores default para as props, com isso evitamos fazer validações direto no método render
-  getDefaultProps () {
-    return {
-      name: 'Desconhecido',
-      lastName: 'Sem sobrenome'
-    }
-  },
+// Usando shorthand notation no lugar de usar o objeto props
+const Title = ({ name, lastName }) => (
+  <h1>Olá { `${name} ${lastName}` }</h1>
+)
 
-  render () {
-    return (
-      <h1>Olá {this.props.name + ' ' + this.props.lastName}!</h1>
-    )
-  }
-})
+// Método estático da nossa função
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastName: 'Sem sobrenome'
+}
+
+// const Title = React.createClass({
+//   // O getDefaultProps define valores default para as props, com isso evitamos fazer validações direto no método render
+//   getDefaultProps () {
+//     return {
+//       name: 'Desconhecido',
+//       lastName: 'Sem sobrenome'
+//     }
+//   },
+
+//   render () {
+//     return (
+//       <h1>Olá {this.props.name + ' ' + this.props.lastName}!</h1>
+//     )
+//   }
+// })
 
 export default Title
